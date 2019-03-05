037a4b612d4e652ff85ff16da934f9256176562e
josephriosio <blankworldwide@gmail.com>
2019-03-05 13:08:53 -0800

    completed 4 event listeners

--- js/index.js
+++ js/index.js
@@ -14,15 +14,31 @@ navContainer.addEventListener("mouseover", function() {
   }
 });
 
-// Event Listener : 2
+//Event Listener : 2
+const intro = document.querySelector(".intro");
+
+intro.addEventListener("dblclick", function() {
+  TweenLite.to(intro, 2.5, { ease: Bounce.easeOut, y: -500 });
+});
+
+// Event Listener : 3
 const imgContent = document.querySelectorAll("img");
 
 for (let i = 0; i < imgContent.length; i++) {
   let img = imgContent[i];
-  console.log(img);
-  img.addEventListener("click", function() {
+  img.addEventListener("dblclick", function(event) {
     img.style.border = "5px solid #89cff0";
+    event.stopPropagation();
   });
 }
 
-//Event Listener : 3
+// Event Listener : 4
+const pTags = document.querySelectorAll("p");
+
+for (let i = 0; i < pTags.length; i++) {
+  let p = pTags[i];
+  console.log(p);
+  p.addEventListener("mouseover", function(e) {
+    p.style.color = "#89cff0";
+  });
+}
