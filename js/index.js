eb0e0077dc44d3ace45e9b405eade2efc2ef7071
josephriosio <blankworldwide@gmail.com>
2019-03-05 12:41:46 -0800

    completed 2 event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+
+// Event Listener : 1
+const navContainer = document.querySelector(".nav-container");
+const links = document.querySelectorAll("a");
+
+navContainer.addEventListener("mouseover", function() {
+  navContainer.style.backgroundColor = "#89cff0";
+  navContainer.style.color = "white";
+  for (let i = 0; i < links.length; i++) {
+    let link = links[i];
+
+    link.style.color = "white";
+  }
+});
+
+// Event Listener : 2
+const imgContent = document.querySelectorAll("img");
+
+for (let i = 0; i < imgContent.length; i++) {
+  let img = imgContent[i];
+  console.log(img);
+  img.addEventListener("click", function() {
+    img.style.border = "5px solid #89cff0";
+  });
+}
+
+//Event Listener : 3
